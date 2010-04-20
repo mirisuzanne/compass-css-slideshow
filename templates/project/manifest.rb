60abb4563857411d41237c6f15febeecf98885cf
Chris Eppstein <chris@eppsteins.net>
2010-04-19 18:48:43 -0700

    Add some template metadata for better integration with the compass command line.

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,3 +1,16 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet '_slideshow.scss', :media => 'screen, projection'
 html 'slideshow.html'
+
+description "a css-only slideshow"
+
+help %Q{
+Installs some html and a stylesheet that you can use directly
+or refer to as an example.
+}
+
+welcome_message %Q{
+Please refer to the slideshow.html file to see how the markup should be structured.
+And to the slideshow stylesheet partial to see how to use the library and apply it to your markup.
+}
+
